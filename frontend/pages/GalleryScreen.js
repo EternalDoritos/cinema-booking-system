0c70158d02ab2d3910a6e68155288f0e75125bc6
ngjunrong <118435186+ngjunrong@users.noreply.github.com>
2023-04-20 03:14:50 +0800

    made GalleryScreen responsive

--- frontend/pages/GalleryScreen.js
+++ frontend/pages/GalleryScreen.js
@@ -1,3 +1,98 @@
+// import React from "react";
+// import Link from "next/link";
+
+// export const getStaticProps = async () => {
+//   const res = await fetch("http://localhost:5000/movie");
+//   const data = await res.json();
+
+//   return {
+//     props: { movies: data },
+//   };
+// };
+
+// const DisplayGallery = ({ movies }) => {
+//   return (
+//     <div>
+//       <h1 className="text-white text-center text-4xl py-10 font-bold uppercase tracking-wider">
+//         Movie List
+//       </h1>
+//       <div className="mt-8 grid grid-cols-3 gap-10 bg-black">
+//         {movies.map((movie) => (
+//           <div key={movie.id} className="bg-gray-800 rounded-lg p-4 ">
+//             <Link href={"/SynopsisScreen"}>
+//               <div className="card relative">
+//                 <div className="bg-black-300">
+//                   <img
+//                     className="object-cover h-70 w-96 "
+//                     src={movie.image}
+//                     alt={movie.title}
+//                   />
+//                 </div>
+//                 <div className="m-4">
+//                   <h3 className="text-lg font-bold mb-2 text-white">
+//                     {movie.title}
+//                   </h3>
+//                   <p className="text-sm text-gray-400">{movie.description}</p>
+//                 </div>
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DisplayGallery;
+
+// testing responsive
+// import React from "react";
+// import Link from "next/link";
+
+// export const getStaticProps = async () => {
+//   const res = await fetch("http://localhost:5000/movie");
+//   const data = await res.json();
+
+//   return {
+//     props: { movies: data },
+//   };
+// };
+
+// const DisplayGallery = ({ movies }) => {
+//   return (
+//     <div>
+//       <h1 className="text-white text-center text-4xl py-10 font-bold uppercase tracking-wider">
+//         Movie List
+//       </h1>
+//       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-black">
+//         {movies.map((movie) => (
+//           <div key={movie.id} className="bg-gray-800 rounded-lg p-4 ">
+//             <Link href={"/SynopsisScreen"}>
+//               <div className="card relative">
+//                 <div className="bg-black-300">
+//                   <img
+//                     className="object-cover h-70 w-full md:h-52 lg:h-70 md:w-96 lg:w-full"
+//                     src={movie.image}
+//                     alt={movie.title}
+//                   />
+//                 </div>
+//                 <div className="m-4">
+//                   <h3 className="text-lg font-bold mb-2 text-white">
+//                     {movie.title}
+//                   </h3>
+//                   <p className="text-sm text-gray-400">{movie.description}</p>
+//                 </div>
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DisplayGallery;
+
 import React from "react";
 import Link from "next/link";
 
@@ -16,14 +111,14 @@ const DisplayGallery = ({ movies }) => {
       <h1 className="text-white text-center text-4xl py-10 font-bold uppercase tracking-wider">
         Movie List
       </h1>
-      <div className="mt-8 grid grid-cols-3 gap-10 bg-black">
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-black">
         {movies.map((movie) => (
           <div key={movie.id} className="bg-gray-800 rounded-lg p-4 ">
             <Link href={"/SynopsisScreen"}>
               <div className="card relative">
                 <div className="bg-black-300">
                   <img
-                    className="object-cover h-70 w-96 "
+                    className="object-cover h-full w-full lg:h-70 xl:w-96"
                     src={movie.image}
                     alt={movie.title}
                   />
