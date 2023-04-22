480dc4e877e74a03b0d29a4ef4118f8f75d56be1
zhenyang <zhenyang455@gmail.com>
2023-04-22 20:02:41 +0800

    added synopsis update

--- frontend/pages/SynopsisScreen.js
+++ frontend/pages/SynopsisScreen.js
@@ -28,8 +28,11 @@ const DisplaySynopsis = () => {
     return (
       <>
       <div className = "bg-gray-800 grid lg:grid-cols-2 sm:grid-cols-1">
-        <div className = "object-contain xl:h-4/6 xl:w-4/6 lg:h-auto lg:w-96 md:w-3/4">
+        {/* <div className = "object-contain xl:h-4/6 xl:w-4/6 lg:h-auto lg:w-96 md:w-3/4">
         <img src={movie[0].image}></img>
+        </div> */}
+        <div className="object-contain xl:h-4/6 xl:w-4/6 lg:h-auto lg:w-96 md:w-3/4 mx-auto my-auto">
+          <img src={movie[0].image} />
         </div>
         <div>
         <h1 className="xl:text-5xl sm:text-4xl font-semibold text-gray-200">{movie[0].title}</h1>
