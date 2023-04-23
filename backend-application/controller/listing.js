df1805e07ba241d021a414cd8a4c864bb279c547
Lim Pei Ming <peiminglm@gmail.com>
2023-04-23 20:51:49 +0800

    added patch and delete for movie listing

--- backend-application/controller/listing.js
+++ backend-application/controller/listing.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 
 const Listing = require("../models/listing");
-const { restart } = require("nodemon");
 
 //@desc     GET all the current listings of the movie
 //@route    GET/listing/:id
@@ -37,5 +36,54 @@ exports.postListing = asyncHandler(async (req, res) => {
     date: req.body.date,
     time: req.body.time,
   });
+  res.status(200).json(listing);
+});
+
+//@desc     PATCH seating arrangement
+//@route    PATCH/listing/seat
+//@access   private
+exports.patchListing = asyncHandler(async (req, res) => {
+  const arr = req.body.booked;
+  const existingSeat = await Listing.findOne(
+    { _id: req.body.id },
+    { seating: 1, _id: 0 }
+  );
+  for (let ele of arr) {
+    existingSeat.seating[ele] = true;
+  }
+
+  const listing = await Listing.findByIdAndUpdate(
+    { _id: req.body.id },
+    {
+      seating: existingSeat.seating,
+    }
+  );
+  res.status(200).json(listing);
+});
+
+//@desc     PATCH listing details
+//@route    PATCH/listing
+//@access   private
+
+exports.patchListingAll = asyncHandler(async (req, res) => {
+  const listing = await Listing.findByIdAndUpdate(
+    { _id: req.body.id },
+    {
+      movie: req.body.movie,
+      cinema: req.body.cinema,
+      seating: req.body.seating,
+      date: req.body.date,
+      time: req.body.time,
+    }
+  );
+
+  res.status(200).json(listing);
+});
+
+//@desc     DELETE listing details
+//@route    DELETE/listing
+//@access   private
+exports.deleteListing = asyncHandler(async (req, res) => {
+  const listing = await Listing.findByIdAndDelete({ _id: req.body.id });
   res.status(200).json(listing);
 });
--- backend-application/routes/listing.js
+++ backend-application/routes/listing.js
@@ -9,4 +9,11 @@ router.get("/:id", listingController.getListing);
 
 //listing -> POST
 router.post("/", listingController.postListing);
+
+//listing -> PATCH
+router.patch("/seat", listingController.patchListing);
+router.patch("/", listingController.patchListingAll);
+
+//listing -> DELETE
+router.delete("/", listingController.deleteListing);
 module.exports = router;
