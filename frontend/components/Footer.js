1c8ec9f58fba229ab3a8d41f6e060a52c33fe82d
ngjunrong <118435186+ngjunrong@users.noreply.github.com>
2023-05-09 19:38:00 +0800

    footer links are now clickable, just a raw draft

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,28 +1,7 @@
 import styles from "../styles//Footer.module.css";
+import Link from "next/link";
 import Image from "next/image";
-
-// function Footer() {
-//   return (
-//     <footer className={styles.footer}>
-//       <div className={styles.container}>
-//         <div className={styles.logo}>
-//           <Image src="/logo.png" alt="Cinema Logo" width={200} height={110} />
-//           <h3>GoldenRizz Copyright 2023</h3>
-//         </div>
-//         <div className={styles.menu}>
-//           <ul>
-//             <li>Home</li>
-//             <li>Movies</li>
-//             <li>About Us</li>
-//             <li>Contact Us</li>
-//           </ul>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
+import aboutUs from "../pages/footerComponents/aboutUs";
 
 function Footer() {
   return (
@@ -32,36 +11,72 @@ function Footer() {
           <div className={styles.column}>
             <h4>About GoldenRizz</h4>
             <ul>
-              <li>About Us</li>
-              <li>Careers</li>
-              <li>Corporate Sales</li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/aboutUs">About Us</Link>
+              </li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/faq"> FAQ</Link>
+              </li>
             </ul>
           </div>
           <div className={styles.column}>
             <h4>Connect with us</h4>
             <ul>
-              <li>Contact Us</li>
-              <li>Feedback</li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/contactUs">Contact Us</Link>
+              </li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/feedback">Feedback</Link>
+              </li>
             </ul>
           </div>
           <div className={styles.column}>
-            <h4>GoldenRizz Movie Extras</h4>
+            <h4>Social Media</h4>
             <ul>
-              <li>Gift Vouchers</li>
-              <li>Promotions</li>
+              <li>
+                {" "}
+                <Link href="https://www.instagram.com/gvmovieclub/">
+                  Instagram
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link href="https://www.facebook.com/gvmovieclub">
+                  Facebook
+                </Link>
+              </li>
             </ul>
           </div>
           <div className={styles.column}>
             <h4>Useful Links</h4>
             <ul>
-              <li>Terms of Use</li>
-              <li>Privacy Policy</li>
-              <li>Sitemap</li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/termsOfUse">Terms of Use</Link>
+              </li>
+              <li>
+                {" "}
+                <Link href="/footerComponents/privacyPolicy">
+                  Privacy Policy
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="GV Logo" width={200} height={110} />
+          <Image
+            src="/logo.png"
+            alt="GoldenRizz Logo"
+            width={200}
+            height={110}
+          />
+          <p className="text-gray-400 text-sm">
+            © 2023 GoldenRizz Site. All rights reserved.
+          </p>{" "}
         </div>
       </div>
     </footer>
