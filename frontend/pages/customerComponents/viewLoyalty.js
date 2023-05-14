aa1a235f72c4583ed1c36f7b8c3ef3a3acad7ea0
ngjunrong <118435186+ngjunrong@users.noreply.github.com>
2023-05-15 01:31:24 +0800

    raw css for customer's view loyalty points

--- frontend/pages/customerComponents/viewLoyalty.js
+++ frontend/pages/customerComponents/viewLoyalty.js
@@ -2,26 +2,40 @@ import React from "react";
 import { useContext } from "react";
 import { Context } from "../../store/context";
 import { useState, useEffect } from "react";
-
+import Head from "next/head";
 
 const DisplayLoyalty = () => {
   const [currentUser, setCurrentUser] = useContext(Context);
 
   return (
     <div>
-    {!currentUser && (
-      <h1> There's no user information to display</h1>
-    )}
-    {currentUser && (
-    <div>
-    <label>View loyalty</label>
-    <div> User Name: {currentUser.username}</div>
-    <div> User Type: {currentUser.userType} </div>
-    <div> Loyalty Points: {currentUser.loyaltyPoints}</div>
+      <Head>
+        <title>View Loyalty Points</title>
+      </Head>
+      {!currentUser && (
+        <h1 className="text-white text-center text-4xl py-10 font-bold uppercase tracking-wide">
+          {" "}
+          There's no user information to display
+        </h1>
+      )}
+      {currentUser && (
+        <div className="text-center">
+          <label className="text-white  text-4xl py-10 font-bold uppercase tracking-wider">
+            View loyalty
+          </label>
+          <div className="mt-10 font-bold text-2xl underline"> User Name</div>
+          <div className="text-2xl">{currentUser.username}</div>
+          <div className="mt-6 font-bold text-2xl underline"> User Type</div>
+          <div className="text-2xl">{currentUser.userType}</div>
+          <div className="mt-6 font-bold text-2xl underline">
+            {" "}
+            Loyalty Points
+          </div>
+          <div className="text-2xl">{currentUser.loyaltyPoints}</div>
+        </div>
+      )}
     </div>
-    )} 
-    </div>
-  )
+  );
 };
 
 export default DisplayLoyalty;
