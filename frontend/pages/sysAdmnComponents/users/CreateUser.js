001ab5d9ba6e8409f0edd9a7ad6370e896485e35
Elroy Chua <elroymx@gmail.com>
2023-05-15 20:33:21 +0800

    added emailjs dependency; added placeholder send email function in create user component

--- frontend/pages/sysAdmnComponents/users/CreateUser.js
+++ frontend/pages/sysAdmnComponents/users/CreateUser.js
@@ -1,37 +1,97 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Head from "next/head";
+
 const CreateUser = () => {
-  const [username, setUsername] = useState("");
-  const [usertype, setUsertype] = useState("");
+  const form = useRef();
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState("");
   const [customertype, setCustomertype] = useState("");
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
   };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
   const handleUserTypeChange = (e) => {
-    setUsertype(e.target.value);
+    setUserType(e.target.value);
   };
   const handleCustomerTypeChange = (e) => {
     setCustomertype(e.target.value);
   };
+  const createUser = async (e) => {
+    e.preventDefault();
+    const createUser = await fetch("http://localhost:5000/auth/register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName: userName,
+        email: email,
+        userType: userType,
+        customerType: customertype ? customertype : "",
+      }),
+    });
+    if (createUser.status === 200) {
+      window.alert("User created successfully, redirecting to log in page");
+      router.push("/UserLogInScreen");
+    } else window.alert("Error creating account");
+  };
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm("gmail", "template_980iw3t", form.current, "F2Ax9knazMafwkb4r")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <>
       <Head>
-        <title>Create Users</title>
+        <title>Create User</title>
       </Head>
       <h1 class="text-white text-center text-4xl pt-10 font-bold uppercase tracking-wider">
         Cinema Booking System Users
       </h1>
-      <p class="text-white text-center text-base pt-2 pb-10  tracking-wider">
+      <p class="text-white text-center text-base pt-2 pb-8  tracking-wider">
         Functions: Create new user(manager, staff, customer)
       </p>
       <div>
         <div className="mb-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="PUT">
+            <form
+              className="space-y-6"
+              action="#"
+              method="PUT"
+              // onSubmit={sendEmail}
+            >
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-400"
+                >
+                  Full name
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    onChange={handleUserNameChange}
+                    placeholder="Name"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
+                  />
+                </div>
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -45,7 +105,7 @@ const CreateUser = () => {
                     name="email"
                     type="email"
                     onChange={handleEmailChange}
-                    placeholder="email"
+                    placeholder="Email"
                     autoComplete="email"
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
                   />
@@ -53,25 +113,7 @@ const CreateUser = () => {
               </div>
               <div>
                 <label
-                  htmlFor="username"
-                  className="block text-sm font-medium text-gray-400"
-                >
-                  User Name
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    onChange={handleUsernameChange}
-                    placeholder="username"
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="usertype"
+                  htmlFor="userType"
                   className="block text-sm font-medium text-gray-400"
                 >
                   User Type
@@ -82,12 +124,12 @@ const CreateUser = () => {
                     name="userType"
                     type="text"
                     onChange={handleUserTypeChange}
-                    placeholder="usertype"
+                    placeholder="User Type (Manager/ Staff/ Customer)"
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
                   />
                 </div>
               </div>
-              {usertype === "customer" ? (
+              {userType?.toLowerCase() === "customer" ? (
                 <div>
                   <label
                     htmlFor="email"
@@ -111,7 +153,7 @@ const CreateUser = () => {
                 <button
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent  shadow-sm text-sm  bg-amber-300 hover:bg-amber-500 text-black font-bold rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  //   onClick={updateUser}
+                  onClick={createUser}
                 >
                   Create new user
                 </button>
