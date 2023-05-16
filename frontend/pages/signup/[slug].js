2cf03558e559de1c3cf6d45b9ddffe1b96f44eff
Elroy Chua <elroymx@gmail.com>
2023-05-17 00:10:39 +0800

    some signup page changes

--- frontend/pages/signup/[slug].js
+++ frontend/pages/signup/[slug].js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import Link from "next";
+// import { useRouter } from "next/router";
+// import Link from "next/link";
 export async function getServerSideProps(context) {
   const pid = context.params.slug;
-  console.log(pid);
+  // console.log(pid);
   const res = await fetch(
     `http://localhost:5000/auth/getUserByUsername/${pid}`
   );
@@ -15,6 +15,12 @@ export async function getServerSideProps(context) {
   };
 }
 const Signup = ({ user }) => {
+  console.log("User ID: ", user?._id);
+  // let userToEdit = {
+  //   username: user.username,
+  // };
+  // const [username, setUsername] = useState(`{userToEdit?.username}`);
+
   return <div></div>;
 };
 export default Signup;
