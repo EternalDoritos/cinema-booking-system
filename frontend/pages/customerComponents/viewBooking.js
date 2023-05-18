09806666f659047d38e5f9dfff13734d5c687b32
zhenyang <zhenyang455@gmail.com>
2023-05-18 18:07:27 +0800

    Added a fix where view booking crashes

--- frontend/pages/customerComponents/viewBooking.js
+++ frontend/pages/customerComponents/viewBooking.js
@@ -26,10 +26,18 @@ const DisplayBooking = () => {
   };
 
   const formatTime = (time) => {
-    const timeString = time.toString().padStart(4, "0");
-    const formattedTime = new Date(`2000-01-01T${timeString.substring(0, 2)}:${timeString.substring(2, 4)}`);
-    return formattedTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-  }
+    if (time) {
+      const timeString = time.toString().padStart(4, "0");
+      const formattedTime = new Date(
+        `2000-01-01T${timeString.substring(0, 2)}:${timeString.substring(2, 4)}`
+      );
+      return formattedTime.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    }
+    return "";
+  };
 
   const getSeatLabel = (index) => {
     const row = Math.floor(index / 5) + 65; // ASCII code for 'A' is 65
@@ -50,76 +58,59 @@ const DisplayBooking = () => {
           </h1>
         )}
         {currentUser && (
-        <div className="text-center">
-        <label className="text-white text-4xl py-10 font-bold uppercase tracking-wider">
-          Movie Bookings
-        </label>
-        <div className="mt-9">
-          {seats.map((info) => (
-            <div key={info.movieList._id} className="my-4 border border-gray-300 p-4 ml-6 mr-6">
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <p className="text-lg font-semibold">Title:</p>
-                  <p>{info.movieList.movie.title}</p>
-                </div>
-                <div>
-                  <p className="text-lg font-semibold">Location:</p>
-                  <p>{info.movieList.cinema.location}</p>
-                </div>
-                <div>
-                  <p className="text-lg font-semibold">Date:</p>
-                  <p>{formatDate(info.movieList.date)}</p>
-                </div>
-                <div>
-                  <p className="text-lg font-semibold">Time:</p>
-                  <p>{formatTime(info.movieList.time)}</p>
-                </div>
-                <div className="grid grid-col 2">
-                  <div>
-                  <p className="text-lg font-semibold">Booked seats:</p>
+          <div className="text-center">
+            <label className="text-white text-4xl py-10 font-bold uppercase tracking-wider">
+              Movie Bookings
+            </label>
+            <div className="mt-9">
+              {seats.length === 0 && (
+                <p className="text-white text-center text-lg">
+                  User did not book any movies yet
+                </p>
+              )}
+              {seats.map((info) => (
+                <div
+                  key={info.movieList?._id}
+                  className="my-4 border border-gray-300 p-4 ml-6 mr-6"
+                >
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <p className="text-lg font-semibold">Title:</p>
+                      <p>{info.movieList?.movie?.title}</p>
+                    </div>
+                    <div>
+                      <p className="text-lg font-semibold">Location:</p>
+                      <p>{info.movieList?.cinema?.location}</p>
+                    </div>
+                    <div>
+                      <p className="text-lg font-semibold">Date:</p>
+                      <p>{formatDate(info.movieList?.date)}</p>
+                    </div>
+                    <div>
+                      <p className="text-lg font-semibold">Time:</p>
+                      <p>{formatTime(info.movieList?.time)}</p>
+                    </div>
+                    <div className="grid grid-col 2">
+                      <div>
+                        <p className="text-lg font-semibold">Booked seats:</p>
+                      </div>
+                      <div>
+                        {info.seating.map((seat, seatIndex) => (
+                          <span
+                            key={seatIndex}
+                            className="text-white bg-blue-500 px-2 py-1 rounded ml-2"
+                          >
+                            {getSeatLabel(seat)}
+                            {seatIndex !== info.seating.length - 1 && ""}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    {info.seating.map((seat, seatIndex) => (
-                      <span key={seatIndex} className="text-white bg-blue-500 px-2 py-1 rounded ml-2">
-                        {getSeatLabel(seat)}
-                        {seatIndex !== info.seating.length - 1 && ""}
-                      </span>
-                    ))}
-                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      
-      
-        
-        
-          // <div className="text-center">
-          //   <label className="text-white  text-4xl py-10 font-bold uppercase tracking-wider">
-          //     Movie Bookings
-          //   </label>
-          //   <div className="mt-9">
-          //     {seats.map((info) => (
-          //       <div key={info.movieList._id}>
-          //         <span> </span>
-          //         <p>Title: {info.movieList.movie.title}</p>
-          //         <p>Location: {info.movieList.cinema.location}</p>
-          //         <p>Date: {formatDate(info.movieList.date)}</p>
-          //         <p>Time: {info.movieList.time}</p>
-          //         Booked seats:{" "}
-          //         {info.seating.map((seat, seatIndex) => (
-          //           <span key={seatIndex}>
-          //             {getSeatLabel(seat)}
-          //             {seatIndex !== info.seating.length - 1 && ", "}
-          //           </span>
-          //         ))}
-          //         <div className="mb-8"> </div>
-          //       </div>
-          //     ))}
-          //   </div>
-          // </div>
+          </div>
         )}
       </div>
     </div>
@@ -127,3 +118,4 @@ const DisplayBooking = () => {
 };
 
 export default DisplayBooking;
+
