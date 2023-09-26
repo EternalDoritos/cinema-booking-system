4a06f6f4ecf17ef0cdb0ab02ffe3c7f8b7615cb1
Elroy Chua <elroymx@gmail.com>
2023-09-27 01:57:01 +0800

    fix: manager component report fixes

--- frontend/pages/managerComponents/report/dateReport.js
+++ frontend/pages/managerComponents/report/dateReport.js
@@ -1,28 +1,28 @@
 function DisplayReport({ report }) {
+  if (!report || report.length === 0) {
+    return <h1 className="text-center text-2xl m-2">No reports available</h1>;
+  }
+
   const totalRevenue = report[report.length - 1];
   const movies = [...report];
   movies.pop();
-  if (report.length === 1)
-    return <h1 className="text-center text-2xl m-2">No reports available</h1>;
-  else {
-    return (
-      <div>
-        <h1 className="text-center text-4xl m-2 text-bold m-4">Date Report</h1>
-        <div className="m-4 text-xl">
-          {movies.map((ele) => (
-            <p key={Math.random()} className="text-center">
-              Movie Name:&nbsp;{ele.movieName}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              Time:
-              {ele.movieTime}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Revenue:{ele.revenue}
-            </p>
-          ))}
-        </div>
-        <h2 className="text-center text-bold text-3xl">
-          Grand Total:&nbsp;&nbsp;&nbsp;{totalRevenue?.grandTotalDate ?? 0}
-        </h2>
+
+  return (
+    <div>
+      <h1 className="text-center text-4xl m-2 font-bold m-4">Date Report</h1>
+      <div className="m-4 text-xl">
+        {movies.map((ele) => (
+          <p key={Math.random()} className="text-center">
+            Movie Name:&nbsp;{ele.movieName}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Time:
+            {ele.movieTime}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Revenue:{ele.revenue}
+          </p>
+        ))}
       </div>
-    );
-  }
+      <h2 className="text-center font-bold text-3xl">
+        Grand Total:&nbsp;&nbsp;&nbsp;{totalRevenue?.grandTotalDate ?? 0}
+      </h2>
+    </div>
+  );
 }
 
 export default DisplayReport;
--- frontend/pages/managerComponents/report/movieReport.js
+++ frontend/pages/managerComponents/report/movieReport.js
@@ -1,13 +1,16 @@
 function DisplayReport({ report }) {
+  if (!report || report.length === 0) {
+    return <h1 className="text-center text-2xl m-2">No reports available</h1>;
+  }
+
   const totalRevenue = report[report.length - 1];
   const movies = [...report];
   movies.pop();
-  if (report.length === 1)
-    return <h1 className="text-center text-2xl m-2">No reports available</h1>;
-  else {
+
+  if (report.length === 1) {
     return (
       <div>
-        <h1 className="text-center text-4xl m-2 text-bold m-4">Movie Report</h1>
+        <h1 className="text-center text-4xl m-2 font-bold m-4">Movie Report</h1>
         <div className="m-4 text-xl">
           {movies.map((ele) => (
             <p key={Math.random()} className="text-center">
@@ -17,7 +20,7 @@ function DisplayReport({ report }) {
             </p>
           ))}
         </div>
-        <h2 className="text-center text-bold text-3xl">
+        <h2 className="text-center font-bold text-3xl">
           Grand Total:&nbsp;&nbsp;&nbsp;{totalRevenue?.grandTotalMovie ?? 0}
         </h2>
       </div>
