502a5be3e04065013ee90ccc4363685983a71ebf
Elroy Chua <elroymx@gmail.com>
2023-09-26 23:42:07 +0800

    fix: add domain links to next config; update image in gallery

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,4 +9,17 @@ module.exports = {
   publicRuntimeConfig: {
     apiKey: process.env.API_KEY,
   },
+  images: {
+    domains: [
+      "www.posterhub.com.sg",
+      "www.cwfilms.jp",
+      "sportshub.cbsistatic.com",
+      "m.media-amazon.com",
+      "i.ebayimg.com",
+      "www.themoviedb.org",
+      "i2.wp.com",
+      "m.media-amazon.com",
+      "assets-prd.ignimgs.com",
+    ],
+  },
 };
--- frontend/pages/GalleryScreen.js
+++ frontend/pages/GalleryScreen.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
-import Images from "next/image";
+import Image from "next/image";
 import { useContext } from "react";
 import { Context } from "../store/context";
 //import useGlobalStore from "../store/store";
@@ -25,31 +25,37 @@ const DisplayGallery = ({ movies }) => {
         Movie List
       </h1>
       <div className="mb-8 ml-12 mr-12 mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-black border-8 border-black">
-        {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className="bg-gray-800 rounded-lg p-4 cursor-pointer"
-          >
-            <Link passHref={`/SynopsisScreen?movieId=${movie._id}`}>
-              <div className="card relative">
-                <div className="bg-black-300 flex items-center justify-center">
-                  <Image
-                    className="object-cover h-full w-full lg:h-70 xl:w-96"
-                    src={movie.image}
-                    alt={movie.title}
-                    // style={{ objectFit: "cover" }} // Add this line
-                  />
+        {movies.map((movie) => {
+          console.log(movie.image);
+          return (
+            <div
+              key={movie.id}
+              className="bg-gray-800 rounded-lg p-4 cursor-pointer"
+            >
+              <Link href={`/SynopsisScreen?movieId=${movie._id}`}>
+                <div className="card relative">
+                  <div className="bg-black-300 flex items-center justify-center">
+                    <Image
+                      className="object-cover h-full w-full lg:h-70 xl:w-96"
+                      src={movie.image}
+                      alt={movie.title}
+                      width={300}
+                      height={400}
+                      // layout="fill"
+                      style={{ objectFit: "cover" }} // Add this line
+                    />
+                  </div>
+                  <div className="m-4">
+                    <h3 className="text-lg font-bold mb-2 text-white">
+                      {movie.title}
+                    </h3>
+                    <p className="text-sm text-gray-400">{movie.description}</p>
+                  </div>
                 </div>
-                <div className="m-4">
-                  <h3 className="text-lg font-bold mb-2 text-white">
-                    {movie.title}
-                  </h3>
-                  <p className="text-sm text-gray-400">{movie.description}</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
